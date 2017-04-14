ab44d4b4823e107b894c781f2d4eff61a3357948
seesea <seesealonely@gmail.com>
2017-04-14 18:13:07 +0800

    v1

---
+++ 554.BrickWall.h
@@ -0,0 +1,44 @@
+/*
+There is a brick wall in front of you. The wall is rectangular and has several rows of bricks. The bricks have the same height but different width. You want to draw a vertical line from the top to the bottom and cross the least bricks.
+
+The brick wall is represented by a list of rows. Each row is a list of integers representing the width of each brick in this row from left to right.
+
+If your line go through the edge of a brick, then the brick is not considered as crossed. You need to find out how to draw the line to cross the least bricks and return the number of crossed bricks.
+
+You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
+
+Example:
+
+Input: 
+[[1,2,2,1],
+ [3,1,2],
+ [1,3,2],
+ [2,4],
+ [3,1,2],
+ [1,3,1,1]]
+Output: 2
+Explanation: 
+
+Note:
+
+    The width sum of bricks in different rows are the same and won't exceed INT_MAX.
+    The number of bricks in each row is in range [1,10,000]. The height of wall is in range [1,10,000]. Total number of bricks of the wall won't exceed 20,000. 
+*/
+
+#include"head.h"
+class Solution {
+public:
+    int leastBricks(vector<vector<int> >& wall) {
+	map<int,int> m;
+	 int res=wall.size();
+	for(int i=0;i<wall.size();++i)
+		for(int j=0,width=0;j<wall[i].size()-1;++j)	
+		res=min(res,((int)wall.size()-(++m[width+=wall[i][j]]))); 
+	return res;
+    }
+private:
+	inline int mymin(int a,int b)
+	{
+		return (a)<(b)?(a):(b);
+	}
+};
