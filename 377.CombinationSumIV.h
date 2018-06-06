079fd92b61d98089971c1f33cba9ba650d985327
seesea <seesealonely@gmail.com>
2018-06-07 00:56:48 +0800

    v1

--- 377.CombinationSumIV.h
+++ 377.CombinationSumIV.h
@@ -1,5 +1,5 @@
 /*
- Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
+   Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
 
 Example:
 
@@ -31,13 +31,25 @@ int firstWay(vector<int> &nums,int target)
 	if(target==0) return 1;
 	int res=0;
 	for(int i=0;i<nums.size();i++)
-	if(target>=nums[i])
-	res+=firstWay(nums,target-nums[i]);
+		if(target>=nums[i])
+			res+=firstWay(nums,target-nums[i]);
 	return res;
 }
+int origindp(vector<int> & nums,int target)
+{
+	vector<vector<int> > dp(nums.size()+1,vector<int> (target+1,0));
+
+	for(int i=0;i<nums.size()+1;i++)
+		dp[i][0]=1;
+	for(int i=1;i<nums.size()+1;i++)
+		for(int j=1;j<target+1;j++)
+			if(i-nums[j]>=dp[i][j])
+				dp[i][j]+=dp[i][i-nums[j]];		
+	return dp[nums.size()][target];
+}
 class Solution {
-public:
-    int combinationSum4(vector<int>& nums, int target) {
-     return  firstWay(nums,target); 
-    }
+	public:
+		int combinationSum4(vector<int>& nums, int target) {
+			return  origindp(nums,target); 
+		}
 };
--- 91.DecodeWays.h
+++ 91.DecodeWays.h
@@ -1,31 +1,43 @@
 /*
- A message containing letters from A-Z is being encoded to numbers using the following mapping:
+   A message containing letters from A-Z is being encoded to numbers using the following mapping:
 
-'A' -> 1
-'B' -> 2
-...
-'Z' -> 26
+   'A' -> 1
+   'B' -> 2
+   ...
+   'Z' -> 26
 
-Given an encoded message containing digits, determine the total number of ways to decode it.
+   Given an encoded message containing digits, determine the total number of ways to decode it.
 
-For example,
-Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+   For example,
+   Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
 
-The number of ways decoding "12" is 2. 
-*/
-
-#include"head.h"
-
-class Solution {
-public:
-    int numDecodings(string s) {
+   The number of ways decoding "12" is 2. 
+   */
+int opmizition(string s)
+{
 	int ir1=1,ir2=1;
 	for(int i=1;i<s.size();++i)
 	{
-	if(s[i]==''0') ir1=1;	
-	if(s[i-1]=='1'||s[i-1]=='2'&&s[i]<='6')
-	else ir2=ir1;	
+		if(s[i]==''0') ir1=1;	
+		if(s[i-1]=='1'||s[i-1]=='2'&&s[i]<='6')
+		else ir2=ir1;	
 	}
 	return ir1;
-    }
+}
+int origin(string s)
+{
+	vector<int> dp(s.size(),0);
+	dp[0]=s[0]!=0?1:0;
+	dp[1]=s[1]!=0?1:0;
+	for(int i=2;i<=s.size();i++)
+	{
+		if(s[i-2]=='1'||s[i-2]=='2'&&s[i-1]<='6')
+	}
+}
+#include"head.h"
+
+class Solution {
+	public:
+		int numDecodings(string s) {
+		}
 };
