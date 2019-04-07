0eed02816b86f7e84a17cdb543e4585486f1b82e
seesea <seesealonely@gmail.com>
2019-04-07 22:36:00 +0800

    easy

---
+++ 1013.PartitionArrayIntoThreePartsWithEqualSum.cc
@@ -0,0 +1,46 @@
+/*
+Given an array A of integers, return true if and only if we can partition the array into three non-empty parts with equal sums.
+
+Formally, we can partition the array if we can find indexes i+1 < j with (A[0] + A[1] + ... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1])
+
+ 
+
+Example 1:
+
+Input: [0,2,1,-6,6,-7,9,1,2,0,1]
+Output: true
+Explanation: 0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+
+Example 2:
+
+Input: [0,2,1,-6,6,7,9,-1,2,0,1]
+Output: false
+
+Example 3:
+
+Input: [3,3,6,5,-2,2,5,1,-9,4]
+Output: true
+Explanation: 3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+
+ 
+
+Note:
+
+    3 <= A.length <= 50000
+    -10000 <= A[i] <= 10000
+
+*/
+
+#include"1013.PartitionArrayIntoThreePartsWithEqualSum.h"
+
+int main()
+{
+	Solution s;
+	vector<int> v={0,2,1,-6,6,-7,9,1,2,0,1};
+	cout<<s.canThreePartsEqualSum(v)<<endl;
+	v.clear();v={0,2,1,-6,6,7,9,-1,2,0,1};
+	cout<<s.canThreePartsEqualSum(v)<<endl;
+	v.clear();v={3,3,6,5,-2,2,5,1,-9,4};
+	cout<<s.canThreePartsEqualSum(v)<<endl;
+	return 0;
+}
