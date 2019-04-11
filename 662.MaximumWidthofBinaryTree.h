9cf4cde208a9de2d9c73318997c1d903383b442a
seesea <seesealonely@gmail.com>
2019-04-11 23:28:45 +0800

    medium tree

---
+++ 662.MaximumWidthofBinaryTree.h
@@ -0,0 +1,90 @@
+/*
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+
+Example 2:
+
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+
+Example 3:
+
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+
+Example 4:
+
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+
+
+Note: Answer will in the range of 32-bit signed integer.
+
+*/
+
+#include"head.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+       vector<int> lefts;
+	int res=0;
+	dfs(root,1,0,lefts,res); 
+	return res;
+    }
+    void dfs(TreeNode* root,int id,int depth,vector<int> &lefts,int &res) {
+	if(!root) return ;        
+	if(depth>=lefts.size()) lefts.push_back(id);
+	res=max(res,id-lefts[depth]+1);
+	dfs(root->left,id*2,depth+1,lefts,res);
+	dfs(root->right,id*2+1,depth+1,lefts,res);
+    }
+};
