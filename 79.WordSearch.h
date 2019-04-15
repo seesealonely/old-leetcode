876f14dfc4cc3ebb849329261331dd1ff16e6f02
seesea <seesealonely@gmail.com>
2019-04-16 02:00:49 +0800

    dfs

--- 79.WordSearch.h
+++ 79.WordSearch.h
@@ -20,13 +20,31 @@ Given word = "ABCB", return false.
 
 #include"head.h"
 
-bool dfs(vector<vector<char> >& board, string word) 
-{
-		
-}
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        
+    bool exist(vector<vector<char>>& board,string word) {
+	row=board.size();
+	col=board[0].size();
+	for(int i=0;i<row;i++)
+		for(int j=0;j<col;j++)
+			if(dfs(board,word.c_str(),i,j)) 
+			return true; 
+	return false;
     }
+bool dfs(vector<vector<char> >& board,const char * word,int x,int y) 
+{
+	if(x<0||y<0||x>=row||y>=col||*word!=board[x][y]||board[x][y]=='\0')
+		return false;
+	if(*(word+1)=='\0')
+		 return true;
+	char temp=board[x][y];
+	board[x][y]='\0';
+	if(dfs(board,word+1,x+1,y)||dfs(board,word+1,x-1,y)||dfs(board,word+1,x,y+1)||dfs(board,word+1,x,y-1))
+	return true;
+	board[x][y]=temp;
+	return false;
+}
+private:
+	int row;
+	int col;
 };
