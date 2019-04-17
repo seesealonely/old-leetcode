84a52243accd0a08e15441268820884778dcdc33
seesea <seesealonely@gmail.com>
2019-04-18 03:03:22 +0800

    easy

--- 628.MaximumProductofThreeNumbers.h
+++ 628.MaximumProductofThreeNumbers.h
@@ -23,30 +23,10 @@ Note:
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-	int max1=INT_MIN,max2=INT_MIN,max3=INT_MIN,min1=INT_MAX,min2=INT_MAX;
-	for(int i=0;i<nums.size();++i)
-	{
-		if(i>max1)
-		{
-			max3=max2;
-			max2=max1;
-			max1=nums[i];
-		}
-		else if(i>max2)
-		{
-			max3=max2;
-			max2=nums[i];
-		}
-		else if(i>max3)
-			max3=nums[i];
-		if(i<min1)
-		{
-			min2=min1;
-			min1=nums[i];
-		}
-		else if(i<min2)
-			min2=nums[i];
-	}        
-		return max(max1*max2*max3,max1*min1*min2);
+	sort(nums.begin(),nums.end());
+	int n=nums.size();
+	int l=nums[0]*nums[1]*nums[n-1];
+	int r=nums[n-1]*nums[n-2]*nums[n-3];
+	return l<r?r:l;
     }
 };
