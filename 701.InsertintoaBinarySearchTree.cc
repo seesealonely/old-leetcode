4a221be517fb5aaee2f2c8ed021d04c1d20ad112
seesea <seesealonely@gmail.com>
2019-04-28 18:50:21 +0800

    medium traval

---
+++ 701.InsertintoaBinarySearchTree.cc
@@ -0,0 +1,53 @@
+/*
+Given the root node of a binary search tree (BST) and a value to be inserted into the tree, insert the value into the BST. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Note that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+For example, 
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+And the value to insert: 5
+
+You can return this binary search tree:
+
+         4
+       /   \
+      2     7
+     / \   /
+    1   3 5
+
+This tree is also valid:
+
+         5
+       /   \
+      2     7
+     / \   
+    1   3
+         \
+          4
+
+*/
+
+#include"701.InsertintoaBinarySearchTree.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+int main()
+{
+	Solution s;
+	show(s.insertIntoBST(buildTree(),5));
+	return 0;
+}
