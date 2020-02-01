a33ad9df9a8f6033efe361b2671b3241630b77e1
seesea <seesealonely@gmail.com>
2020-02-02 03:18:28 +0800

    easy

---
+++ 1304.FindNUniqueIntegersSumuptoZero.cc
@@ -0,0 +1,39 @@
+/*
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: [-7,-1,1,3,4]
+Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+
+Example 2:
+
+Input: n = 3
+Output: [-1,0,1]
+
+Example 3:
+
+Input: n = 1
+Output: [0]
+
+ 
+
+Constraints:
+
+    1 <= n <= 1000
+
+*/
+
+#include"1304.FindNUniqueIntegersSumuptoZero.h"
+
+int main()
+{
+	Solution s;
+	show(s.sumZero(5));
+	show(s.sumZero(3));
+	show(s.sumZero(1));
+	return 0;
+}
