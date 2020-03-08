d4fa4f8c541127f5864629af77fe63bf643d3880
seesea <seesealonely@gmail.com>
2020-03-09 02:32:29 +0800

    medium two pointers

---
+++ 986.IntervalListIntersections.h
@@ -0,0 +1,76 @@
+/*
+Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+(Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
+
+ 
+
+Example 1:
+
+Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
+
+ 
+
+Note:
+
+    0 <= A.length < 1000
+    0 <= B.length < 1000
+    0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
+
+*/
+
+#include"head.h"
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+
+#include"head.h"
+/*
+class Solution {
+public:
+    vector<Interval> intervalIntersection(vector<Interval>& A, vector<Interval>& B) {
+	vector<Interval> res;
+	Interval in;
+	if(!A.size()||!B.size()||A.empty()||B.empty()) return res;
+	int a=A.size(),b=B.size(),i=0,j=0;
+	while(i<a&&j<b)
+	{
+		in.start=max(A[i].start,B[j].start);
+		in.end=min(A[i].end,B[j].end);
+		if(in.end>=in.start) res.push_back(in);
+		if(A[i].end==in.end) i++;
+		if(B[j].end==in.end) j++;
+	}
+	return res;
+    }
+};
+*/
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+         vector<vector<int>> res;
+	vector<int> in(2);
+	if(!A.size()||!B.size()||A.empty()||B.empty()) return res;
+	int a=A.size(),b=B.size(),i=0,j=0;
+	while(i<a&&j<b)
+	{
+		in[0]=max(A[i][0],B[j][0]);
+		in[1]=min(A[i][1],B[j][1]);
+		if(in[1]>=in[0]) res.push_back(in);
+		if(A[i][1]==in[1]) i++;
+		if(B[j][1]==in[1]) j++;
+	}
+	return res;
+    }
+};
