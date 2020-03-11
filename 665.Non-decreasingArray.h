e99d99accbf6c08c01557c61f23a52ca047ad166
seesea <seesealonely@gmail.com>
2020-03-12 04:10:49 +0800

    easy greedy

--- 665.Non-decreasingArray.h
+++ 665.Non-decreasingArray.h
@@ -23,15 +23,22 @@ Note: The n belongs to [1, 10,000].
 class Solution {
 	public:
 		bool checkPossibility(vector<int>& nums) {
-			bool   change=false;
-			for(int i=1;i<nums.size();i++)
+			return inputModified(nums);
+			return inputModified(nums);
+		}
+		bool inputModified(vector<int>& nums) {
+			int modified=0;
+			for(int i=1;i<nums.size()&&modified<=1;i++)
 			{
 				if(nums[i]<nums[i-1])
 				{
-					if(change++) return false;
-					if(i-2>=0&&nums[i-2]>nums[i])continue;
+					modified++;
+					if(i<2||nums[i-2]<=nums[i]) nums[i-1]=nums[i];
+					else nums[i]=nums[i-1];
 				}
-			} 	       
-			return true;
+			}
+			return  modified<=1;
+		}
+		bool inputNoModified(vector<int>& nums) {
 		}
 };
