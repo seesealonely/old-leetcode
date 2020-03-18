d7d1d0d29d97fd54e7e529370bdba8dbe9090ede
seesea <seesealonely@gmail.com>
2020-03-18 21:35:22 +0800

    medium

---
+++ 398.RandomPickIndex.h
@@ -0,0 +1,48 @@
+/*
+Given an array of integers with possible duplicates, randomly output the index of a given target number. You can assume that the given target number must exist in the array.
+
+Note:
+The array size can be very large. Solution that uses too much extra space will not pass the judge.
+
+Example:
+
+int[] nums = new int[] {1,2,3,3,3};
+Solution solution = new Solution(nums);
+
+// pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+solution.pick(3);
+
+// pick(1) should return 0. Since in the array only nums[0] is equal to 1.
+solution.pick(1);
+
+*/
+
+#include"head.h"
+
+class Solution {
+public:
+    Solution(vector<int>& nums) {
+       this->nums=nums;
+    }
+    
+    int pick(int target) {
+    int res=-1,len=nums.size(),mod=0;;
+	for(int i=0;i<len;i++)
+	{
+		if(nums[i]==target)
+		{
+			mod++;
+			if(rand()%mod==0) res=i;
+		}
+	}
+	return res;
+    }
+private:
+	vector<int> nums;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
