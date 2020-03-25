04f07bc8f1d1828510aa5f91267fcb89d32abbbe
seesea <seesealonely@gmail.com>
2020-03-25 12:16:08 +0800

    medium dp

--- 377.CombinationSumIV.h
+++ 377.CombinationSumIV.h
@@ -26,48 +26,24 @@ What limitation we need to add to the question to allow negative numbers?
 */
 
 #include"head.h"
-int dfs(vector<int> &nums,int target)
-{
-	if(!target)
-	return 1;
-	int	res=0;	
-	for(int i=0;i<nums.size();i++)
-	if(target-nums[i]>=0)
-	res+=dfs(nums,target-nums[i]);
-	return res;
-}
-int recursivedp(vector<int> &nums,int target,vector<int> &dp)
-{
-	if(dp[target]!=-1)
-	return dp[target];
-	int res=0;
-	for(int i=0;i<nums.size();i++)
-		if(target-nums[i]>=0)
-		res+=recursivedp(nums,target-nums[i],dp);
-	dp[target]=res;
-	return res;
-}
-int dp(vector<int> & nums,int target)
-{
-	vector<int>  dp(target+1,0);
-	dp[0]=1;
-	for(int i=1;i<target+1;i++)
-	for(int j=0;j<nums.size();j++)
-	{
-			if(i-nums[j]>=0)
-			dp[i]+=dp[i-nums[j]];
-	}
-	return dp[target];
-}
+
 class Solution {
 	public:
 		int combinationSum4(vector<int>& nums, int target) {
-//			return  dfs(nums,target); 
-/*
-			vector<int> dp(target+1,-1);
+			return dp(nums,target);
+		}
+
+		int dp(vector<int>& nums, int target) {
+			vector<int> dp(target+1,0);
 			dp[0]=1;
-			return  recursivedp(nums,target,dp); 
-*/
-			return  dp(nums,target); 
+			for(int i=1;i<=target;i++)
+				for(int j=0;j<nums.size();j++)
+				{
+					{
+						if(i-nums[j]>=0)
+						dp[i]+=dp[i-nums[j]];
+					}
+				}
+			return dp[target];
 		}
 };
