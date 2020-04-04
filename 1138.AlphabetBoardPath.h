14831f0b2dc264d4f795f23f2e38395542c88aa4
seesea <seesealonely@gmail.com>
2020-04-05 00:39:43 +0800

    medium string

---
+++ 1138.AlphabetBoardPath.h
@@ -0,0 +1,53 @@
+/*
+On an alphabet board, we start at position (0, 0), corresponding to character board[0][0].
+
+Here, board = ["abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"], as shown in the diagram below.
+
+We may make the following moves:
+
+    'U' moves our position up one row, if the position exists on the board;
+    'D' moves our position down one row, if the position exists on the board;
+    'L' moves our position left one column, if the position exists on the board;
+    'R' moves our position right one column, if the position exists on the board;
+    '!' adds the character board[r][c] at our current position (r, c) to the answer.
+
+(Here, the only positions that exist on the board are positions with letters on them.)
+
+Return a sequence of moves that makes our answer equal to target in the minimum number of moves.  You may return any path that does so.
+
+ 
+
+Example 1:
+
+Input: target = "leet"
+Output: "DDR!UURRR!!DDD!"
+
+Example 2:
+
+Input: target = "code"
+Output: "RR!DDRR!UUL!R!"
+
+ 
+
+Constraints:
+
+    1 <= target.length <= 100
+    target consists only of English lowercase letters.
+
+*/
+#include"head.h"
+
+class Solution {
+public:
+    string alphabetBoardPath(string target) {
+	string res;	
+	int x=0,y=0;
+	for(int i=0;i<target.size();i++)
+	{
+		int r=(target[i]-'a')/5;int c=(target[i]-'a')%5;
+		res+=string(max(0,x-r),'U')+string(max(0,c-y),'R')+string(max(0,y-c),'L')+string(max(0,r-x),'D')+string("!");	
+		x=r,y=c;
+	}
+	return res;
+    }
+};
