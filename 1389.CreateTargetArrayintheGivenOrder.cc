b2fdefa71418dff03194f7f20a0229315cdfd9b3
seesea <seesealonely@gmail.com>
2020-04-11 07:36:37 +0800

    easy insert

---
+++ 1389.CreateTargetArrayintheGivenOrder.cc
@@ -0,0 +1,63 @@
+/*
+Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+    Initially target array is empty.
+    From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+    Repeat the previous step until there are no elements to read in nums and index.
+
+Return the target array.
+
+It is guaranteed that the insertion operations will be valid.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+Explanation:
+nums       index     target
+0            0        [0]
+1            1        [0,1]
+2            2        [0,1,2]
+3            2        [0,1,3,2]
+4            1        [0,4,1,3,2]
+
+Example 2:
+
+Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
+Output: [0,1,2,3,4]
+Explanation:
+nums       index     target
+1            0        [1]
+2            1        [1,2]
+3            2        [1,2,3]
+4            3        [1,2,3,4]
+0            0        [0,1,2,3,4]
+
+Example 3:
+
+Input: nums = [1], index = [0]
+Output: [1]
+
+ 
+
+Constraints:
+
+    1 <= nums.length, index.length <= 100
+    nums.length == index.length
+    0 <= nums[i] <= 100
+    0 <= index[i] <= i
+*/
+
+#include"1389.CreateTargetArrayintheGivenOrder.h"
+
+int main()
+{
+	Solution s;
+	vector<int> n,i;
+	n.push_back(0);n.push_back(1);n.push_back(2);n.push_back(3);n.push_back(4);
+	i.push_back(0);i.push_back(1);i.push_back(2);i.push_back(2);i.push_back(1);
+	show(s.createTargetArray(n,i));
+	return 0;
+}
